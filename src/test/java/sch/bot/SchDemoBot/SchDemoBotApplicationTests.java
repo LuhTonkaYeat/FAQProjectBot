bd69ad48039de1cca3568b8facff46b8d1fe7a24
yeat <125463688+LuhTonkaYeat@users.noreply.github.com>
2024-05-10 13:18:46 +0300

    Add files via upload

---
+++ src/test/java/sch/bot/SchDemoBot/SchDemoBotApplicationTests.java
@@ -0,0 +1,13 @@
+package sch.bot.SchDemoBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchDemoBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
